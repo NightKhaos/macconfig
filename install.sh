e7e408820cf957e8c371e0fe5712349057247f26
nightkhaos <nightkhaos+gpg@gmail.com>
2018-04-13 16:46:14 +1000

    init commit

---
+++ install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -e
+
+# This script will install the needed prerequisites and configuration files
+
+## Script Setup
+BREW_PACKAGES_PATH=$(dirname $0)/packages.brew
+CASK_PACKAGES_PATH=$(dirname $0)/cask.brew
+PYTHON_REQUIREMENTS=$(dirname $0)/python.requirements
+POWERSHELL_MODULES_PATH=$(dirname $0)/powershell.packages
+NPM_MODULES_PATH=$(dirname $0)/packages.npm
+ZSHRC_SOURCE=$(dirname $0)/zshrc
+SECURE_MAC_CONFIG_SCRIPT=$HOME/.secmacconfig/install.sh
+SSH_CONFIG=$(dirname $0)/config.ssh
+
+# Make sure we're in an interactive shell
+if ! tty
+then
+    echo "Must be in an interactive TTY" >&2
+    exit 1
+fi
+
+# Install Homebrew:
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Upgrade Homebrew package
+brew upgrade
+
+# Install required packages
+[ -e "$BREW_PACKAGES_PATH" ] && grep -Ev '(#.*$)|(^$)' "$BREW_PACKAGES_PATH" | xargs brew install
+
+# Install cask packages
+[ -e "$CASK_PACKAGES_PATH" ] && grep -Ev '(#.*$)|(^$)' "$CASK_PACKAGES_PATH" | xargs brew cask install
+
+# Install Required Python3 packages
+[ -e "$PYTHON_REQUIREMENTS" ] && sudo pip3 install -r "$PYTHON_REQUIREMENTS"
+
+# Install Powershell packages
+[ -e "$POWERSHELL_MODULES_PATH" ] && grep -Ev '(#.*$)|(^$)' "$POWERSHELL_MODULES_PATH" | xargs -I {} -n1 pwsh -c "Install-Module -Name {} -AllowClobber -Force"
+
+# Install NPM packages
+
+[ -e "$NPM_MODULES_PATH" ] && grep -Ev '(#.*$)|(^$)' "$NPM_MODULES_PATH" | xargs -n1 npm install -g
+# Install git libraries
+[ -d "$HOME/.vim" ] || git clone --recursive ssh://github.com/NightKhaos/vimrc.git "$HOME/.vim"
+[ -d "$HOME/.oh-my-zsh" ] || git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
+[ -d "$HOME/.scm_breeze" ] || git clone git://github.com/ndbroadbent/scm_breeze.git "$HOME/.scm_breeze"
+
+# Install configuration files
+[ -e "$ZSHRC_SOURCE" ] && ( [ -e "$HOME/.zshrc" ] || ln -s "$ZSHRC_SOURCE" "$HOME/.zshrc" )
+[ -e "$HOME/.vimrc" ] || ln -s "$HOME/.vim/vimrc" "$HOME/.vimrc"
+
+# Install config files that need to be appendable
+mkdir -p "$HOME/.s"
+mkdir -p "$HOME/.ssh"
+[ -e "$SSH_CONFIG" ] && ( [ -e "$HOME/.ssh/config" ] || cp "$SSH_CONFIG" "$HOME/.ssh/config" )
+
+# Install Secure Mac Config Settings
+[ -e "$SECURE_MAC_CONFIG_SCRIPT" ] && $SECURE_MAC_CONFIG_SCRIPT
---
+++ readme.md
@@ -0,0 +1,17 @@
+# What is this?
+
+This is NightKhaos' Mac OS X (10.12) configuration script. It includes:
+
+- Homebrew Packages to be installed
+- Setting up Python
+- Setting up Oh-My-Zsh and SCM Breeze
+
+NightKhaos uses this to initialise a new Mac OS X machine to get the terminal just the way he links it by simply running:
+
+    cd
+    git clone https://github.com/NightKhaos/macconfig.git .macconfig
+    .maconfig/install.sh
+
+## There are some extra files?
+
+The extra files are useful artefacts needed to configure the environment, like Terminal themes, etc.
